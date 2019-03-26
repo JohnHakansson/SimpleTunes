21f3cca48187ab62bbcc8a5a5490ee025c3cd909
John Håkansson <john.hs.hakansson@outlook.com>
2019-03-26 11:00:02 +0100

    Nu funkar ljudet

--- John_Demo.java
+++ John_Demo.java
@@ -1,5 +1,7 @@
 package simpleTunes;
 
+import java.io.File;
+
 import javafx.application.*;
 import javafx.event.EventHandler;
 import javafx.scene.*;
@@ -15,17 +17,18 @@ public class John_Demo extends Application {
 	private Color white = Color.WHITE;
 	private Color aquamarine = Color.AQUAMARINE;
 	private Color violet = Color.BLUEVIOLET;
-	private Rectangle rect = new Rectangle(150, 200, 100, 100);
+	private Rectangle rect = new Rectangle(150, 200, 120, 100);
 	private Circle circle = new Circle(200, 365, 50, violet);
 	private Polygon triangle = new Polygon();
 	private Line line500 = new Line(500, 0, 500, 600);
 	private Scene scene;
 	private MediaPlayer mediaPlayer;
+	private String file = "file:///C://Users//Ginis//Music//KennyRogersTheGambler.mp3";
 	private String musicFile1 = "sounds/17569__danglada__c-major.wav";
 	private String musicFile2 = "sounds/17567__danglada__a-major.wav";
 	private String musicFile3 = "sounds/17568__danglada__b-major.wav";
-	private Media[] soundFiles = { new Media(musicFile1),
-			new Media(musicFile2), new Media(musicFile3)};
+	private Media[] soundFiles = { new Media(new File(musicFile1).toURI().toString()),
+			new Media(new File(musicFile2).toURI().toString()), new Media(new File(musicFile3).toURI().toString()), new Media(file)};
 	private double orgSceneX;
 	private double orgSceneY;
 	private double orgTranslateX;
@@ -53,7 +56,7 @@ public class John_Demo extends Application {
 		scene.setFill(black);
 
 		rect.setOnMousePressed(getMouseEvent());
-		rect.setOnMouseDragged(getMouseDragged(0));
+		rect.setOnMouseDragged(getMouseDragged(3));
 
 		triangle.setOnMousePressed(getMouseEvent());
 		triangle.setOnMouseDragged(getMouseDragged(1));
@@ -73,8 +76,11 @@ public class John_Demo extends Application {
 	public EventHandler<MouseEvent> getMouseEvent() {
 		EventHandler<MouseEvent> circleOnMousePressedEventHandler = new EventHandler<MouseEvent>() {
 
-			@Override
 			public void handle(MouseEvent t) {
+				orgSceneX = 0;
+				orgSceneY = 0;
+				orgTranslateX = 0;
+				orgTranslateY = 0;
 				orgSceneX = t.getSceneX();
 				orgSceneY = t.getSceneY();
 				orgTranslateX = ((Shape) (t.getSource())).getTranslateX();
@@ -86,10 +92,16 @@ public class John_Demo extends Application {
 	}
 
 	public EventHandler<MouseEvent> getMouseDragged(int sound) {
+		
+		newTranslateX = 0;
+		newTranslateY = 0;
+		
+		
 		EventHandler<MouseEvent> circleOnMouseDraggedEventHandler = new EventHandler<MouseEvent>() {
 
-			@Override
+			
 			public void handle(MouseEvent t) {
+				
 				double offsetX = t.getSceneX() - orgSceneX;
 				double offsetY = t.getSceneY() - orgSceneY;
 				newTranslateX = orgTranslateX + offsetX;
@@ -97,13 +109,14 @@ public class John_Demo extends Application {
 
 				((Shape) (t.getSource())).setTranslateX(newTranslateX);
 				((Shape) (t.getSource())).setTranslateY(newTranslateY);
+				
+				if(newTranslateX > 100) {
+					mediaPlayer = new MediaPlayer(soundFiles[sound]);
+					mediaPlayer.play();
+				}
 			}
 		};
 		
-		if(newTranslateY > line500.getEndY()) {
-			mediaPlayer = new MediaPlayer(soundFiles[sound]);
-			mediaPlayer.play();
-		}
 		return circleOnMouseDraggedEventHandler;
 
 	}
