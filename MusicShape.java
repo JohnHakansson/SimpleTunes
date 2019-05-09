4f4ce67ecfff25c3700aaad5351541c9cd1e77a4
Tom <tom.lanhed@gmail.com>
2019-05-09 11:14:02 +0200

    kommenterat musicShape

--- MusicShape.java
+++ MusicShape.java
@@ -7,6 +7,13 @@ import javafx.scene.media.Media;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Shape;
 
+/**
+ * 
+ * Abstract class containing one JavaFX shape, an audioplayer and media object and methods for manipulating these.
+ * 
+ * @author Tom Lanhed Sivertsson, John Håkansson
+ *
+ */
 public abstract class MusicShape {
 	private AudioClip mediaPlayer;
 	private Media sound;
@@ -24,41 +31,85 @@ public abstract class MusicShape {
 	private int column;
 
 	private Color color;
-
+	
+	/**
+	 * 
+	 * @param color Color of the shape
+	 * @param sound Sound to be connected with the shape
+	 */
 	public MusicShape(Color color, Media sound) {
 		setSound(sound);
 		this.color = color;
-		System.out.println(color.getBlue());
 
 	}
-
+	
+	/**
+	 * 
+	 * Setter for the media object
+	 * 
+	 * @param sound The new sound
+	 */
 	private void setSound(Media sound) {
 		mediaPlayer = new AudioClip(sound.getSource());
 		this.sound = sound;
 	}
-
+	
+	/**
+	 * 
+	 * Getter for the media object
+	 * 
+	 * @return sound
+	 */
 	public Media getSounds() {
 		return sound;
 	}
-
+	
+	/**
+	 *  plays the sound
+	 */
 	public void play() {
 		mediaPlayer.play();
 	}
-
+	
+	/**
+	 * stops playing the sound
+	 */
 	public void stop() {
 		mediaPlayer.stop();
 	}
-
+	
+	/**
+	 * @return the type of musicshape
+	 */
 	public String toString() {
 		return "MusicShape";
 	}
-
+	
+	/**
+	 * 
+	 * @return the shape object
+	 */
 	public abstract Shape getShape();
-
+	
+	/**
+	 * 
+	 * @param x The value of the shape objects setLayoutX
+	 */
 	public abstract void setLayoutX(double x);
-
+	
+	
+	/**
+	 * 
+	 * @param y The value of the shape objects setLayoutY
+	 */
 	public abstract void setLayoutY(double y);
-
+	
+	/**
+	 * 
+	 * Generates an eventhandler for playing when clicking a shape
+	 * 
+	 * @return EventHandler
+	 */
 	public EventHandler<MouseEvent> getMouseEvent() {
 		EventHandler<MouseEvent> OnMouseClicked = new EventHandler<MouseEvent>() {
 
@@ -72,7 +123,13 @@ public abstract class MusicShape {
 
 		return OnMouseClicked;
 	}
-
+	
+	/**
+	 * 
+	 * 	Generates an eventhandler for updating x, y coordinates when pressing a shape
+	 * 
+	 * @return EventHandler
+	 */
 	public EventHandler<MouseEvent> getMouseEventPressed() {
 		EventHandler<MouseEvent> onMousePressed = new EventHandler<MouseEvent>() {
 
@@ -89,6 +146,12 @@ public abstract class MusicShape {
 		return onMousePressed;
 	}
 
+	/**
+	 * 
+	 * 	Generates an eventhandler for updating x, y coordinates when dragging a shape
+	 * 
+	 * @return EventHandler
+	 */
 	public EventHandler<MouseEvent> getMouseEventDragged() {
 		EventHandler<MouseEvent> onMouseDragged = new EventHandler<MouseEvent>() {
 
@@ -108,35 +171,80 @@ public abstract class MusicShape {
 
 		return onMouseDragged;
 	}
-
+	
+	/**
+	 * 
+	 * Sets the boolean flagging whether or not a shape has been placed in the grid
+	 * 
+	 * @param bool
+	 */
 	public void setPlaced(boolean bool) {
 		placedInGrid = bool;
 	}
-
+	
+	/**
+	 * 
+	 * Returns the boolean flagging whether or not a shape has been placed in the grid
+	 * 
+	 * @return
+	 */
 	public boolean getPlaced() {
 		return placedInGrid;
 	}
-
+	
+	/**
+	 * 
+	 * Sets which row the shape has been placed in
+	 * 
+	 * @param row
+	 */
 	public void setRow(int row) {
 		this.row = row;
 	}
-
+	
+	/**
+	 * 
+	 * Sets which column the shape has been placed in
+	 * 
+	 * @param column
+	 */
 	public void setColumn(int column) {
 		this.column = column;
 	}
-
+	
+	/**
+	 * 
+	 * Returns the row the shape has been placed in
+	 * 
+	 * @return row
+	 */
 	public int getRow() {
 		return row;
 	}
-
+	
+	/**
+	 * 
+	 * Returns the row the shape has been placed in
+	 * 
+	 * @return column
+	 */
 	public int getColumn() {
 		return column;
 	}
-
+	
+	/**
+	 * 
+	 * Returns the color of the shape
+	 * 
+	 * @return color
+	 */
 	public Color getColor() {
 		return color;
 	}
-
+	
+	/**
+	 * Will set all event handlers to null
+	 */
 	public abstract void nullifyEventHandlers();
 
 }
